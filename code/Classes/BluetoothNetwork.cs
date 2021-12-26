0d97e8649ce7dbe9c54f193705111f8294484484
infinite41i <57639431+infinite41i@users.noreply.github.com>
2021-12-26 20:56:48 +0330

    Create BluetoothNetwork.cs

---
+++ code/Classes/BluetoothNetwork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BluetoothSimulator
+{
+    class BluetoothNetwork
+    {
+        private static Random randomNumberGenerator = new Random();
+        public static int getRandomNumber()
+        {
+            return randomNumberGenerator.Next();
+        }
+    }
+}

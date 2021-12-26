7a1c90236970a66046e55a2347698dc4a6fa9ae8
infinite41i <57639431+infinite41i@users.noreply.github.com>
2021-12-26 20:59:21 +0330

    Update Piconet.cs

--- code/Classes/Piconet.cs
+++ code/Classes/Piconet.cs
@@ -8,5 +8,13 @@ namespace BluetoothSimulator
 {
     class Piconet
     {
+        public Node[] nodes;
+        private int nodeCount = 0;
+        Channel channel = new Channel();
+
+        public void newNode(string name)
+        {
+            nodes[nodeCount] = new Node(name);
+        }
     }
 }

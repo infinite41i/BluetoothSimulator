7e278e4d3080bc6bedc717fde4bd6e3c43915578
infinite41i <57639431+infinite41i@users.noreply.github.com>
2021-12-26 20:59:18 +0330

    Update Node.cs

--- code/Classes/Node.cs
+++ code/Classes/Node.cs
@@ -8,5 +8,21 @@ namespace BluetoothSimulator
 {
     class Node
     {
+        private int BTGUID;
+        private string BTname;
+        private bool master = false;
+        private NodeMode mode;
+
+        public Node(string name)
+        {
+            BTGUID = BluetoothNetwork.getRandomNumber();
+            BTname = name;
+            mode = NodeMode.Standby;
+        }
+
+        public void recievePacket()
+        {
+            //
+        }
     }
 }
